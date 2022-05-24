a7dd96266a50349e88158f543722fdf938ed9655
Dayana Tito <Dayanatitochoque@hotmail.com>
2022-05-24 17:18:11 -0300

    upload

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -4,7 +4,7 @@ import './Home.css';
 function Home(){
     return(
         <>
-            <h1 className='principal'>Home</h1>
+            <h1 className='titulo'>Home</h1>
             <img className='foto' src='https://img.freepik.com/fotos-gratis/imagem-aproximada-em-tons-de-cinza-de-uma-aguia-careca-americana-em-um-fundo-escuro_181624-31795.jpg?w=996&t=st=1653414209~exp=1653414809~hmac=d32fd02e830279064b80a99e80dcecc0aa04ea28cee26cc742bc9b287451e186'/>
         </>
     );
